88752528629fef234355bd188c6a9acae4d6beb7
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-06-17 17:00:57 -0300

    iniciando raking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Ranking extends Component {
+  handleclickLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          onClick={ this.handleclickLogin }
+          data-testid="btn-go-home"
+        >
+          Home
+
+        </button>
       </div>
     );
   }
 }
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
